e4581a27c99b12b970b1a42f5e0e5f983c22ddbe
Vladislav Sabolotny <vladislav.sabolotny@gmail.com>
2021-05-29 22:21:40 +0200

    Add finding my location, permission handling

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,22 +1,39 @@
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Dimensions,
-} from "react-native";
+import React, { useState, useEffect, usePermissions } from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { TabActions } from "@react-navigation/native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import * as Location from "expo-location";
 
 import CustomHeaderButton from "../components/HeaderButton";
 
 const HomeScreen = (navigator) => {
-  const [curLocation, setCurLocation] = useState(null);
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      await Location.requestForegroundPermissionsAsync();
+      setLocation(await Location.getCurrentPositionAsync({}));
+    })();
+  }, []);
+
+  // console.log(location);
 
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} provider={PROVIDER_GOOGLE}></MapView>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 48.15913,
+          longitude: 11.626086,
+          // latitude: location.coords.latitude,
+          // longitude: location.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+
       {/* <View style={styles.truckTile}>
         <Text>Food Truck #1</Text>
       </View> */}
@@ -60,12 +77,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  truckTile: {
-    ...StyleSheet.absoluteFill,
-    width: 100,
-    height: 100,
-    backgroundColor: "blue",
+    // },
+    // truckTile: {
+    //   ...StyleSheet.absoluteFill,
+    //   width: 100,
+    //   height: 100,
+    //   backgroundColor: "blue",
   },
   map: {
     width: Dimensions.get("window").width,
