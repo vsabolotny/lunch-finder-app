d0518f06f97a3dd99fff4dd9f832927a6fc13219
Vladislav Sabolotny <vladislav.sabolotny@gmail.com>
2021-05-29 21:26:02 +0200

    Enable map

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,25 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+} from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { TabActions } from "@react-navigation/native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 import CustomHeaderButton from "../components/HeaderButton";
 
-const HomeScreen = () => {
+const HomeScreen = (navigator) => {
+  const [curLocation, setCurLocation] = useState(null);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.greeting}>Map with Food Trucks</Text>
+      <MapView style={styles.map} provider={PROVIDER_GOOGLE}></MapView>
+      {/* <View style={styles.truckTile}>
+        <Text>Food Truck #1</Text>
+      </View> */}
     </View>
   );
 };
@@ -50,8 +61,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  greeting: {
-    fontFamily: "open-sans",
+  truckTile: {
+    ...StyleSheet.absoluteFill,
+    width: 100,
+    height: 100,
+    backgroundColor: "blue",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
 
